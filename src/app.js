a702a2134cf9e847f385d8ad34c4ed35dd1f6bbe
Jonathan Rodriguez <clubzspadez@gmail.com>
2018-02-21 14:36:11 -0500

    using switch to display 404 pages if the path does not match the exact route

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route}  from 'react-router-dom';
+import { BrowserRouter, Route, Switch }  from 'react-router-dom';
 import App from './components/App';
 import Create from './components/Create';
 import EditPage from './components/EditPage';
@@ -9,14 +9,21 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 // defining our routes in jsx
+const NotFound = () => (
+  <div>
+   404!
+  </div>
+);
+
 const routes = (
   <BrowserRouter>
-    <div>
-    <Route exact={true} path="/" component={App}/>
-    <Route path="/create" component={Create} />
-    <Route path="/edit" component={EditPage} />
-    <Route path="/help" component={HelpPage} />
-    </div>
+    <Switch>
+      <Route exact={true} path="/" component={App}/>
+      <Route path="/create" component={Create} />
+      <Route path="/edit" component={EditPage} />
+      <Route path="/help" component={HelpPage} />
+      <Route component={NotFound} />
+    </Switch>
   </BrowserRouter>
 );
 
